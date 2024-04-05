6fd81739861a31342b0635fe7d720a02d3b765da
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-04-05 09:47:18 +0600

    update

--- 09 Module Intruduction/myproject/myfirstApp/urls.py
+++ 09 Module Intruduction/myproject/myfirstApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 # from views import contact
 from .import views 
 
+#add another project information
 urlpatterns = [
     path("course/",views.course),
     path("about/",views.about),
