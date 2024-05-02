be394c499c5c746ee2c52414d0b08746b55cf7e4
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-05-02 11:44:46 +0600

    update

--- 19 Module Class base views/myproject/posts/views.py
+++ 19 Module Class base views/myproject/posts/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic import UpdateView,DeleteView
 from  django.utils.decorators import method_decorator
 from django.views.generic import DetailView
-
+from . import models, forms
 
 #add post use class base view 
 @method_decorator(login_required, name='dispatch')
@@ -38,26 +38,26 @@ class DeletePost(DeleteView):
     pk_url_kwarg= 'id'
     success_url =reverse_lazy('profile')
     
-    
 class DetailPostView(DeleteView):
-    model =models.Post 
+    model = models.Post 
     form_class = forms.PostForm
-    pk_url_kwarg='id'
+    pk_url_kwarg = 'id'
     template_name = 'post_details.html'
     
     def get_context_data(self, **kwargs):
-        context= super().get_context_data(***kwargs)
+        context = super().get_context_data(**kwargs)
         post = self.object 
-        comments= post.comments.object.all()
-        if self.request.method =='POST':
+        comments = post.comments.all()
+        
+        if self.request.method == 'POST':
             comment_form = forms.CommentForm(data=self.request.POST)
             if comment_form.is_valid():
-                new_comment =comment_form.save(commit=False)
-                new_comment.post =post
+                new_comment = comment_form.save(commit=False)
+                new_comment.post = post
                 new_comment.save()
         else:
-            comment_form= forms.CommentForm()
-        context['comments']=comments
-        context['comment_form'] =comment_form
+            comment_form = forms.CommentForm()
+        
+        context['comments'] = comments
+        context['comment_form'] = comment_form
         return context
-      
