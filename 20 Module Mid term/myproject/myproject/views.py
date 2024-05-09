4d22ce28863f1834b4457af30076bfe7df11d547
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-05-09 19:43:23 +0600

    successfully  comment create

--- 20 Module Mid term/myproject/myproject/views.py
+++ 20 Module Mid term/myproject/myproject/views.py
@@ -1,15 +1,16 @@
 from typing import Any
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from cars import models
 from cars.models import Car
 from cars import forms
 from cars.models import Brand
 from django.views.generic import DetailView
-from  cars.forms import CommentForm
+from cars.forms import CommentForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-    
+
 from django.views.generic import DetailView
+
 
 def home(request, category_slug=None):
     data = models.Car.objects.all()
@@ -29,19 +30,17 @@ def home(request, category_slug=None):
 #     return render(request,'car_details.html',{'car':car})
 
 
-
-
 # class DetailPostView(DetailView):
-#     model = models.Car 
+#     model = models.Car
 #     form_class = forms.CarForm
 #     pk_url_kwarg = 'id'
 #     template_name = 'car_details.html'
-    
+
 #     def get_context_data(self, **kwargs):
 #         context = super().get_context_data(**kwargs)
-#         post = self.object 
+#         post = self.object
 #         comments = post.comments.all()
-        
+
 #         if self.request.method == 'POST':
 #             comment_form = forms.CommentForm(data=self.request.POST)
 #             if comment_form.is_valid():
@@ -50,7 +49,7 @@ def home(request, category_slug=None):
 #                 new_comment.save()
 #         else:
 #             comment_form = forms.CommentForm()
-        
+
 #         context['comments'] = comments
 #         context['comment_form'] = comment_form
 #         return context
@@ -73,28 +72,27 @@ def car_detail(request, car_id):
     return render(request, 'car_detail.html', {'car': car, 'comments': comments, 'form': form})
 
 
-from django.views.generic import DetailView
 class DetailPostView(DetailView):
-    model = models.Car 
+    model = models.Car
     form_class = forms.CarForm
     pk_url_kwarg = 'id'
     template_name = 'car_detail.html'
-    
+
+    def post(self, request, *args, **kwargs):
+        post = self.get_object()
+        comment_form = forms.CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.car = post  # Set the car_id field
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        post = self.object 
+        post = self.object
         comments = post.comments.all()
-        
-        if self.request.method == 'POST':
-            comment_form = forms.CommentForm(data=self.request.POST)
-            if comment_form.is_valid():
-                new_comment = comment_form.save(commit=False)
-                new_comment.post = post
-                new_comment.save()
-        else:
-            comment_form = forms.CommentForm()
-        
+        comment_form = forms.CommentForm()
+
         context['comments'] = comments
         context['comment_form'] = comment_form
         return context
-    
