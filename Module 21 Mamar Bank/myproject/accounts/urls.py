c6fc288fc6aeff2c8362e29d21608e2a856e68cd
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-05-13 11:53:25 +0600

    mamar bank day -1

--- Module 21 Mamar Bank/myproject/accounts/urls.py
+++ Module 21 Mamar Bank/myproject/accounts/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
 
   path('register/',views.UserRegistrationView.as_view(),name='register'),
+  path('login/',views.UserLoginView.as_view(),name='login'),
+  path('logout/',views.UserLoginView.as_view(),name='logout'),
+  path('profile/',views.UserBankAccountUpdateView.as_view(),name='profile'),
 ]
