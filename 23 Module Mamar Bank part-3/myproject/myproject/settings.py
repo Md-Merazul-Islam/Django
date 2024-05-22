301d5fa0d9b4ca3c96ef804000bd0488458d307f
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-05-22 07:56:11 +0600

    update info

--- 23 Module Mamar Bank part-3/myproject/myproject/settings.py
+++ 23 Module Mamar Bank part-3/myproject/myproject/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import dj_database_url
 import os
-
 from pathlib import Path
-
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -20,35 +19,36 @@ SECRET_KEY = env("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 # Your secret key
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
+
+# render Database 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default='postgres://mamarbank_zpah_user:84cdZ5a0mcslc5TPeyHpD4VRyW8Eiiv2@dpg-cp62bdg21fec7389ruf0-a.oregon-postgres.render.com/mamarbank_zpah'
+    )
 }
+
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-zxqd.onrender.com','https://*.127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
