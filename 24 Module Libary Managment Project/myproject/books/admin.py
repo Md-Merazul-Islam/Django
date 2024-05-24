d8f8c32c6b33907c1a818cf38df32921fe795e28
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-05-24 18:39:40 +0600

    library management

--- 24 Module Libary Managment Project/myproject/books/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Book, Comment, Purchase
-
-admin.site.register(Book)
-admin.site.register(Comment)
-admin.site.register(Purchase)
--- 24 Module Libary Managment Project/myproject/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('book-list/', views.BookListView.as_view(), name='book_list'),
-    path('',views.Book_list_by_Category,name='book_list'),
-    path('category/<slug:category_slug>/',views.Book_list_by_Category,name='category_wise_view'),
-
-]
